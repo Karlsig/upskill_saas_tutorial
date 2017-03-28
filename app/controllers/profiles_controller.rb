8c0cce0cf5a6c2fbbd4512a9798bfd7b1a89fa64
Karl Sigurvinsson <kalli94@gmail.com>
2017-03-28 23:16:39 +0000

    added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
     
     #GET to /user/:user_id/profile/new
     def new
-        #render blank profile details form 
+        @profile = Profile.new
     end
     
 end
